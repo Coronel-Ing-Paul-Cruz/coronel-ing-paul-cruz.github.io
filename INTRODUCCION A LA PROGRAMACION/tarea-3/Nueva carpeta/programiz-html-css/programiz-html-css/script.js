29e7bfc69f1b8b124e93ce793820e1a2f0b2c565
Coronel-Ing-Paul-Cruz <125376960+Coronel-Ing-Paul-Cruz@users.noreply.github.com>
2023-06-15 11:09:10 -0400

    2

---
+++ INTRODUCCION A LA PROGRAMACION/tarea-3/Nueva carpeta/programiz-html-css/programiz-html-css/script.js
@@ -0,0 +1,6 @@
+  <script>
+        var nombreDiaSemanaLink = document.getElementById('nombreDiaSemanaLink');
+        nombreDiaSemanaLink.addEventListener('click', function () {
+            this.setAttribute('download', 'NombreDiaSemana.pas');
+        });
+    </script>
